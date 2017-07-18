3303c691be4f0d5410e0d27ebcc8235da5acf492
andela-jchinonso <chinonso.johnson@andela.com>
2017-07-18 12:10:38 +0100

    feature(jwt): implement jwt on client side [Delivers #148328313]

--- public/js/controllers/index.js
+++ public/js/controllers/index.js
@@ -50,6 +50,27 @@ angular.module('mean.system')
                 });
   };
 
+  $scope.signIn = () => {
+    const useremail = $scope.email;
+    const userpassword = $scope.password;
+    const data = {
+      email: useremail,
+      password: userpassword
+    };
+    // console.log(data);
+    $http.post('/api/auth/signin', data)
+    .then((response) => {
+      console.log(data);
+      if (response.data.message === 'successful login') {
+        window.localStorage.setItem('jwt', response.data.token);
+        $location.path('/#!/');
+        $window.location.reload();
+      } else {
+        alert('Wrong email or user already exist');
+      }
+    })
+  };
+
   $scope.logOut = () => {
     window.localStorage.removeItem('jwt');
     $http.get('/signout')
