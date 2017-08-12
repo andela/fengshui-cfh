68e738b72caf82a54c0de85e9c59e1851ff87a22
Andela Developer <andeladeveloper@Andelas-MacBook-ProNurudeen.local>
2017-08-12 13:51:02 +0100

    feat(implementation): Onboard users effectively onto the app
                        - create game_tour controller
                        - create onboard.html view
                        - install intro.js
                        - add intro.js css and js to head.jade and foot.jade respectively
    
                        [Delivers #148328327]

---
+++ public/js/controllers/game_tour.js
@@ -0,0 +1,43 @@
+const GameTourController = ($location) => {
+  const gameTour = introJs();
+  gameTour.setOptions({
+    steps: [{
+      intro: 'Welcome to the game Cards for Humanity, You want to play this game?, then let me take you on a tour.'
+    },
+    {
+      element: '#logo',
+      intro: 'This is Cards for humanity official logo'
+    },
+    {
+      element: '#question-container-outer',
+      intro: 'Game needs a minimum of 3 players to start. Wait for the minimum number of players and start the game.',
+    },
+    {
+      element: '#chat-for-Tour',
+      intro: 'Here, you can chat with other players.',
+      position: 'top'
+    },
+    {
+      element: '#timer-container',
+      intro: 'Choose an answer to the current question. After time out, CZAR then select a favorite answer. whoever submits CZAR\'s favorite answer wins the round.'
+    },
+    {
+      element: '#abandon-game-button',
+      intro: 'Played enough? Click this button to quit the game'
+    },
+    {
+      element: '#tweet-container',
+      intro: 'Share game experience with friends on twitter'
+    },
+    {
+      element: '#answers-container',
+      intro: 'These are the rules of the game',
+    }
+    ]
+  });
+  gameTour.start()
+         .onexit(() => {
+           $location.path('/');
+         });
+};
+angular.module('tour', []).controller('GameTourController', GameTourController);
