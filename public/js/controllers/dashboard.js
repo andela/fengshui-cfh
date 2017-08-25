a717ae0d6afb81f9e27513655731a94cd39278af
noordean <ebroyeem90@gmail.com>
2017-08-25 10:19:26 +0100

    bug(refactoring): refactor staging
    - fix the landing page and gaming screen
    - fix game-log dashboard
    - fix gametour
    - fix email not sending
    
    [Delivers]

---
+++ public/js/controllers/dashboard.js
@@ -0,0 +1,51 @@
+angular.module('mean.system')
+.controller('DashboardController', ['$scope', '$http', ($scope, $http) => {
+  const token = window.localStorage.getItem('jwt');
+  const config = { headers: {
+    Authorization: `Bearer ${token}`,
+    Accept: 'application/json;odata=verbose'
+  } };
+
+  $scope.leaderboardShow = false;
+  $scope.gameHistoryShow = false;
+  $scope.donationsShow = false;
+
+  $scope.showLeaderboard = () => {
+    $http.get('/api/leaderboard', config)
+      .then((response) => {
+        $scope.leaderboard = response.data.leaderBoard;
+        if (response) {
+          $scope.leaderboardShow = true;
+          $scope.gameHistoryShow = false;
+          $scope.donationsShow = false;
+        }
+      });
+  };
+
+  $scope.showGameHistory = () => {
+    $http.get('/api/games/history', config)
+      .then((response) => {
+        $scope.gameHistory = response.data.history;
+        console.log('your game', $scope.gameHistory[0].gamePlayers[0].username);
+        if (response.data.history) {
+          $scope.leaderboardShow = false;
+          $scope.gameHistoryShow = true;
+          $scope.donationsShow = false;
+        }
+      });
+  };
+
+  $scope.showDonations = () => {
+    $http.get('/api/donations', config)
+      .then((response) => {
+        $scope.donations = response.data.donations;
+        if (response) {
+          $scope.donationsShow = true;
+          $scope.leaderboardShow = false;
+          $scope.gameHistoryShow = false;
+        }
+      });
+  };
+
+  $scope.showLeaderboard();
+}]);
--- public/js/controllers/game.js
+++ public/js/controllers/game.js
@@ -1,11 +1,19 @@
 angular.module('mean.system')
-.controller('GameController', ['$scope', 'game', '$timeout', '$location', 'MakeAWishFactsService', '$dialog', '$http', ($scope, game, $timeout, $location, MakeAWishFactsService, $dialog, $http) => {
+.controller('GameController', ['$scope', 'game', '$timeout', '$location', 'MakeAWishFactsService', 'socket', '$dialog', '$anchorScroll', '$http', ($scope, game, $timeout, $location, MakeAWishFactsService, socket, $dialog, $anchorScroll, $http) => {
   $scope.hasPickedCards = false;
   $scope.winningCardPicked = false;
   $scope.showTable = false;
   $scope.modalShown = false;
   $scope.game = game;
   $scope.pickedCards = [];
+  $scope.allUser = [];
+  $scope.messagesList = '';
+  $scope.chatControler = '^';
+  $scope.charactersLeft = 100;
+  $scope.showChatStatus = false;
+  $scope.invitedUsers = [];
+  $scope.friendList = [];
+  $scope.chatter = {};
   let makeAWishFacts = MakeAWishFactsService.getMakeAWishFacts();
   $scope.makeAWishFact = makeAWishFacts.pop();
 
@@ -18,7 +26,6 @@ angular.module('mean.system')
           $scope.hasPickedCards = true;
         } else if (game.curQuestion.numAnswers === 2 &&
           $scope.pickedCards.length === 2) {
-            // delay and send
           $scope.hasPickedCards = true;
           $timeout($scope.sendPickedCards, 300);
         }
@@ -79,7 +86,7 @@ angular.module('mean.system')
   $scope.isPlayer = $index => $index === game.playerIndex;
 
 
-  $scope.isCustomGame = () => !(/^\d+$/).test(game.gameID) && game.state === 'awaiting players';
+  $scope.isCustomGame = () => game.state === 'awaiting players';
 
 
   $scope.isPremium = $index => game.players[$index].premium;
@@ -90,7 +97,7 @@ angular.module('mean.system')
     if (game.winningCardPlayer !== -1 && $index === game.winningCard) {
       return $scope.colors[game.players[game.winningCardPlayer].color];
     }
-    return '#f9f9f9';
+    return 'rgba(255,255,255,0.5)';
   };
 
   $scope.pickWinning = (winningSet) => {
@@ -109,7 +116,7 @@ angular.module('mean.system')
       type: 'info',
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
+      cancelButtonColor: '#41c2ca',
       cancelButtonText: 'Wait a little',
       confirmButtonText: 'Start Game Now'
     }).then(() => {
@@ -117,11 +124,26 @@ angular.module('mean.system')
     });
   };
 
+  $scope.shuffleCards = () => {
+    const card = $(`#${event.target.id}`);
+    card.addClass('animated flipOutY');
+    setTimeout(() => {
+      $scope.startNextRound();
+      card.removeClass('animated flipOutY');
+      $('#czarModal').modal('hide');
+    }, 500);
+  };
+
+  $scope.startNextRound = () => {
+    if ($scope.isCzar()) {
+      game.startNextRound();
+    }
+  };
+
   $scope.abandonGame = () => {
     game.leaveGame();
     $location.path('/');
   };
-
     // Catches changes to round to update when no players pick card
     // (because game.state remains the same)
   $scope.$watch('game.round', () => {
@@ -140,14 +162,37 @@ angular.module('mean.system')
     if (game.state === 'waiting for czar to decide' && $scope.showTable === false) {
       $scope.showTable = true;
     }
+    if ($scope.isCzar() && game.state === 'czar pick card' && game.table.length === 0) {
+      $('#czarModal').modal({
+        dismissible: false
+      });
+      $('#czarModal').modal('open');
+    }
+    if (game.state === 'game dissolved') {
+      $('#czarModal').modal('close');
+    }
+    if ($scope.isCzar() === false && game.state === 'czar pick card'
+         && game.state !== 'game dissolved'
+         && game.state !== 'awaiting players' && game.table.length === 0) {
+      $scope.czarHasDrawn = 'Wait! Czar is drawing Card';
+    }
+    if (game.state !== 'czar pick card'
+        && game.state !== 'awaiting players'
+         && game.state !== 'game dissolve') {
+      $scope.czarHasDrawn = '';
+    }
     if ($scope.game.state === 'game dissolved' || $scope.game.state === 'game ended') {
       const gameData = { gameId: $scope.game.gameID,
         gameOwner: $scope.game.players[0].username,
         gameWinner: $scope.game.players[game.gameWinner].username,
         gamePlayers: $scope.game.players
       };
-
-      $http.post(`/api/games/${game.gameID}/start`, gameData);
+      const token = window.localStorage.getItem('jwt');
+      const config = { headers: {
+        Authorization: `Bearer ${token}`,
+        Accept: 'application/json;odata=verbose'
+      } };
+      $http.post(`/api/games/${game.gameID}/start`, gameData, config);
     }
   });
 
@@ -173,6 +218,69 @@ angular.module('mean.system')
       }
     }
   });
+  $scope.changeFormOpenIcon = () => {
+    if ($scope.chatControler === '^') {
+      $scope.chatControler = 'v';
+    } else {
+      $scope.chatControler = '^';
+    }
+  };
+
+  $scope.charactersRemaining = (event) => {
+    const myMessage = (event).trim();
+    const messageLength = myMessage.length;
+    $scope.charactersLeft = 100 - messageLength;
+  };
+
+  $scope.submitWithEnter = (event) => {
+    if (event.which === 13) {
+      event.preventDefault();
+      $scope.chat();
+    }
+  };
+
+  $scope.chat = (event) => {
+    const IndividualPlayer = $scope.game.players[$scope.game.playerIndex].username;
+    const playerAvatar = $scope.game.players[$scope.game.playerIndex].avatar;
+    const timeSent = new Date(Date.now()).toLocaleString();
+    const time = (timeSent.split(','))[1];
+    const gameID = $scope.game.gameID;
+    if (event !== '' && event !== undefined) {
+      const newMessage = {
+        sender: IndividualPlayer,
+        message: event,
+        date: time,
+        avater: playerAvatar,
+        gameID
+      };
+      game.chat(newMessage);
+      $scope.message = '';
+      $scope.charactersLeft = 100;
+    }
+  };
+  const displayMessage = (message, modalID) => {
+    $scope.message = message;
+    $(modalID).modal();
+  };
+  $scope.joinName = name => name.split(' ').join('');
+
+  $scope.gotoBottom = () => {
+    $location.hash('bottom');
+    $anchorScroll();
+  };
+
+  $scope.showChat = () => {
+    $scope.showChatStatus = !$scope.showChatStatus;
+  };
+
+  socket.on('reply chat', (data) => {
+    const message = [];
+    Object.keys(data.chat).forEach((key) => {
+      message.push(data.chat[key]);
+    });
+    $scope.messagesList = message;
+    $scope.gotoBottom();
+  });
 
   if ($location.search().game && !(/^\d+$/).test($location.search().game)) {
     game.joinGame('joinGame', $location.search().game);
@@ -181,4 +289,143 @@ angular.module('mean.system')
   } else {
     game.joinGame();
   }
+  $scope.getFriends = () => {
+    const userId = window.user._id;
+    $http.post('/api/friends', { user_id: userId })
+        .success((response) => {
+          $scope.userFriends = response;
+        }, error => error
+        );
+  };
+  $scope.LoadNotifications = () => {
+    const userId = window.user._id;
+    $http.post('/api/notify', { user_id: userId })
+          .success((response) => {
+            $scope.allNotification = response;
+            $scope.count = $scope.allNotification.length;
+          }, error => error
+          );
+  };
+  $scope.readNotifications = (notifyId) => {
+    const userId = window.user._id;
+    $http.post('/api/read', { user_id: userId, notifyId })
+        .success((response) => {
+          if (response.succ) {
+            $scope.LoadNotifications();
+          }
+        }, error => error
+        );
+  };
+  if (window.user !== null) {
+    $scope.LoadNotifications();
+  }
+  $scope.addFriend = (friend, button) => {
+    const friendId = friend._id;
+    const userId = window.user._id;
+    const url = button.target.baseURI;
+    let checkButton;
+    if ($scope.userFriends.includes(friendId)) {
+      checkButton = 'Unfriend';
+    } else {
+      checkButton = 'Addfriend';
+    }
+    $http.post('/friends',
+      {
+        user_id: userId,
+        checkButton,
+        friendId,
+        url
+      })
+          .success((response) => {
+            if (response.succ) {
+              setTimeout(() => {
+                $scope.$apply(() => {
+                  if (response.action === 'addfriend') {
+                    const resultId = response.friendId;
+                    $scope.userFriends.push(resultId);
+                  } else {
+                    const resultId = response.friendId;
+                    const index = $scope.userFriends.indexOf(resultId);
+                    if (index !== -1) {
+                      $scope.userFriends.splice(index, 1);
+                    }
+                  }
+                });
+              }, 100);
+            }
+          });
+  };
+  $scope.sendNotification = (button) => {
+    const friendList = $scope.userFriends;
+    const url = button.target.baseURI;
+    const userName = window.user.name;
+    $http.post('/notify',
+      {
+        userName,
+        friendList,
+        url
+      })
+          .success((response) => {
+            if (response.succ) {
+              response.send('Notification sent Successfully');
+            }
+          });
+  };
+  $scope.invite = (user, button) => {
+    $scope.invitedUsers = JSON.parse(sessionStorage.invitedUsers);
+    if ($scope.invitedUsers.length === 10) {
+      $('[data-toggle="popover"]').popover();
+    }
+
+    if ($scope.invitedUsers.length <= 10) {
+      const inviteButton = document.getElementById(`${button.target.id}`);
+      inviteButton.disabled = true;
+      if ($scope.invitedUsers.indexOf(user.name) === -1) {
+        $scope.invitedUsers.push(user.name);
+        sessionStorage.invitedUsers = JSON.stringify($scope.invitedUsers);
+      }
+    }
+
+    const url = button.target.baseURI;
+    const obj = {
+      url,
+      invitee: user.email,
+      gameOwner: game.players[0].username
+    };
+
+    $http.post('/inviteusers', obj);
+  };
+
+  $scope.getUsers = () => {
+    $http.get('/api/search/users')
+          .success((response) => {
+            $scope.currentUsers = response;
+            displayMessage('', '#users-modal');
+          }, error => error
+          );
+  };
+  $scope.searchUsers = () => {
+    if (!sessionStorage.invitedUsers) {
+      sessionStorage.invitedUsers = JSON.stringify([]);
+    }
+
+    $scope.userMatches = [];
+    $scope.currentUsers.forEach((user) => {
+      const userName = user.name.toLowerCase();
+      const userEmail = user.email.toLowerCase();
+      if (userName.indexOf($scope.searchString.toLowerCase()) !== -1) {
+        $scope.userMatches.push(user);
+      } else if (userEmail === $scope.searchString.toLowerCase()) {
+        $scope.userMatches.push(user);
+      }
+    });
+    $scope.userMatches.forEach((user) => {
+      $scope.invitedUsers = JSON.parse(sessionStorage.invitedUsers);
+      user.disabled = $scope.invitedUsers.includes(user.name);
+    });
+    return $scope.userMatches;
+  };
+
+  $scope.startGameChoice = false;
+  $scope.showInviteButton = false;
 }]);
